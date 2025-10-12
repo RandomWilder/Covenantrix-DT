366da72e6e15f61c700963d7b8df559c0c03fcac
Wilder <wilder.netboost@gmail.com>
2025-10-12 13:31:56 +0300

    v1.1.2: fix app launch“

--- covenantrix-desktop/src/components/layout/StatusBar.tsx
+++ covenantrix-desktop/src/components/layout/StatusBar.tsx
@@ -26,7 +26,7 @@ const StatusBar: React.FC = () => {
           <span>{currentTime}</span>
         </div>
         <div className="text-xs">
-          Covenantrix v1.1.1
+          Covenantrix v1.1.2
         </div>
       </div>
     </div>
