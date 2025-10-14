e24899a9ae3e7159c208857bbd0962e199ce1c76
Wilder <wilder.netboost@gmail.com>
2025-10-15 00:56:56 +0300

    v1.1.17: with subscriptions, notification fixes and custom keys“

--- covenantrix-desktop/src/components/layout/StatusBar.tsx
+++ covenantrix-desktop/src/components/layout/StatusBar.tsx
@@ -30,7 +30,7 @@ const StatusBar: React.FC<StatusBarProps> = ({ onProfileClick }) => {
           <span>{currentTime}</span>
         </div>
         <div className="text-xs">
-          Covenantrix v1.1.16
+          Covenantrix v1.1.17
         </div>
         <button
           onClick={onProfileClick}
