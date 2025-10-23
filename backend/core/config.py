9c6a0afcdc13ffd5dcae59e801abe75b17d02e88
Wilder <wilder.netboost@gmail.com>
2025-10-23 19:23:17 +0300

    v1.1.50: another fix to version download progress“

--- backend/core/config.py
+++ backend/core/config.py
@@ -60,7 +60,7 @@ class Settings(BaseSettings):
     
     # App metadata
     app_name: str = "Covenantrix"
-    version: str = "1.1.49"
+    version: str = "1.1.50"
     environment: str = Field("development", env="ENVIRONMENT")
     
     # Database configuration
