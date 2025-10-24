6d65d49736d8dd92dbc8b42ce809951aa9d9f4dc
Wilder <wilder.netboost@gmail.com>
2025-10-24 13:13:25 +0300

    v1.1.58: another fix to version download progress“

--- covenantrix-desktop/src/components/layout/StatusBar.tsx
+++ covenantrix-desktop/src/components/layout/StatusBar.tsx
@@ -30,7 +30,7 @@ const StatusBar: React.FC<StatusBarProps> = ({ onProfileClick }) => {
           <span>{currentTime}</span>
         </div>
         <div className="text-xs">
-          Covenantrix v1.1.57
+          Covenantrix v1.1.58
         </div>
         <button
           onClick={onProfileClick}
