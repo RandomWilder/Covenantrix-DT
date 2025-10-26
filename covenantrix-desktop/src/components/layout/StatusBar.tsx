078105d54b3d056bb87ab7c4fafd40464daed412
Wilder <wilder.netboost@gmail.com>
2025-10-26 08:26:54 +0200

    v1.1.78: macOS build fixes“

--- covenantrix-desktop/src/components/layout/StatusBar.tsx
+++ covenantrix-desktop/src/components/layout/StatusBar.tsx
@@ -30,7 +30,7 @@ const StatusBar: React.FC<StatusBarProps> = ({ onProfileClick }) => {
           <span>{currentTime}</span>
         </div>
         <div className="text-xs">
-          Covenantrix v1.1.77
+          Covenantrix v1.1.78
         </div>
         <button
           onClick={onProfileClick}
